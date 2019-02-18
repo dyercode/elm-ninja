d821736881d81d93fbfc1d3fa48d9cd5b197eb29
Jon Dyer <dyercode@users.noreply.github.com>
2019-02-18 03:34:58 -0500

    Add bootstrap.  Currently probably anything that it needs js for is broken.  importing loading sass from node_modules, but nothing else
    Randomize the subtitle. phew
    
    set up some npm tasks for development.  possibly should look into how to do this with elm or shellscripts. with bootstrap scss needed might be stuck with node package.json anyways.

--- src/Main.elm
+++ src/Main.elm
@@ -1,48 +1,103 @@
 module Main exposing (main)
 
+import Bootstrap.Button as Button
+import Bootstrap.ButtonGroup as ButtonGroup
+import Bootstrap.Grid exposing (..)
 import Browser
-import Html exposing (Html, button, div, text)
+import Html exposing (Html, h1, header, p, text)
+import Html.Attributes exposing (attribute, class)
 import Html.Events exposing (onClick)
+import Random exposing (Generator)
+import Random.List exposing (choose)
 
 
 type alias Model =
-    { count : Int }
+    { count : Int
+    , title : String
+    , subTitle : String
+    }
 
 
-initialModel : Model
-initialModel =
-    { count = 0 }
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( { count = 0
+      , title = "Jon Dyer"
+      , subTitle = ""
+      }
+    , rlist subtitles
+    )
+
+
+subtitles =
+    [ "(A reasonable subtitle) => text"
+    , "(idea) => code"
+    , "idea -> code -> webpage"
+    ]
 
 
 type Msg
     = Increment
     | Decrement
+    | MyString String
 
 
-update : Msg -> Model -> Model
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         Increment ->
-            { model | count = model.count + 1 }
+            ( { model | count = model.count + 1 }
+            , Cmd.none
+            )
 
         Decrement ->
-            { model | count = model.count - 1 }
+            ( { model | count = model.count - 1 }
+            , Cmd.none
+            )
+
+        MyString string ->
+            ( { model | subTitle = string }
+            , Cmd.none
+            )
 
 
-view : Model -> Html Msg
+rlist : List String -> Cmd Msg
+rlist l =
+    Random.generate myGeneration (choose l)
+
+
+myGeneration : ( Maybe String, List String ) -> Msg
+myGeneration tup =
+    case Tuple.first tup of
+        Just str ->
+            MyString str
+
+        Nothing ->
+            MyString ""
+
+
 view model =
-    div []
-        [ button [ onClick Increment ] [ text "+1" ]
-        , div [] [ text <| String.fromInt model.count ]
-        , button [ onClick Decrement ] [ text "-1" ]
+    container [ attribute "id" "container" ]
+        --        [ CDN.stylesheet
+        [ row []
+            [ col []
+                [ header [ class "jumbotron" ]
+                    [ h1 [ class "display-4" ] [ text model.title ]
+                    , p [ class "lead" ] [ text model.subTitle ]
+                    ]
+                ]
+            ]
+        , ButtonGroup.buttonGroup []
+            [ ButtonGroup.button [ Button.primary, Button.attrs [ onClick Increment ] ] [ text "+1" ]
+            , ButtonGroup.button [] [ text <| String.fromInt model.count ]
+            , ButtonGroup.button [ Button.danger, Button.attrs [ onClick Decrement ] ] [ text "-1" ]
+            ]
         ]
 
 
-main : Program () Model Msg
 main =
-    Browser.sandbox
-        { init = initialModel
+    Browser.element
+        { init = init
         , view = view
         , update = update
+        , subscriptions = \_ -> Sub.none
         }
-
