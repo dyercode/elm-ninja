109852a47df9578f4474b99a10fc0851bc3a8bee
Jon D <dyercode@users.noreply.github.com>
2023-02-17 11:01:42 -0500

    elm-format not available by default yet.

--- flake.nix
+++ flake.nix
@@ -14,6 +14,7 @@
           nativeBuildInputs = with pkgs; [
             nodePackages.yarn
             elmPackages.elm-json
+            elmPackages.elm-format
             podman
             buildah
           ];
