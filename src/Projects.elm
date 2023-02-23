df0841685e5648bf0bbe3552814bfb3b6cc524d1
Jon <dyercode@users.noreply.github.com>
2023-02-22 22:43:54 -0500

    copypasta and proofreading.

--- src/Projects.elm
+++ src/Projects.elm
@@ -88,10 +88,11 @@ latest games. Doesn't account natively for terastalizing.""" ]
     , { title = "(A)Chievos"
       , description =
             [ "Achievement tracker for a certain game. Not yet ready for prime time, but I want to talk about it."
-            , """Built for Kubernetes with an Elm frontend and Scala backend. Uses Http4s, Cats, and doobie. With user authentication, integration with third party APIs.
+            , """Built for Kubernetes with an Elm frontend and Scala backend. Uses Http4s, Cats, and doobie.
+                 Currently has user authentication and integration with third party APIs.
                  Achievements themselves are planned to be extensible."""
             ]
-      , links = [ blogLink Lightning ]
+      , links = []
       }
     ]
 
